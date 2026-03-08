9a2979e0a6b386dc684f5f1e1f8243378da861c3
Sorin-Ioan-Alexandru Bîrchi <84462350+SorinAlexB@users.noreply.github.com>
2026-03-08 21:48:56 +0200

    Fix echo statement for macos-sandbox VM startup

--- tart_infra/start_machines.sh
+++ tart_infra/start_machines.sh
@@ -2,4 +2,4 @@
 tart run linux-sandbox --no-graphics &
 echo "Started linux-sandbox VM on IP $(tart ip linux-sandbox)"
 tart run macos-sandbox --no-graphics &
-echo "Started linux-sandbox VM on IP $(tart ip macos-sandbox)"
+echo "Started macos-sandbox VM on IP $(tart ip macos-sandbox)"
